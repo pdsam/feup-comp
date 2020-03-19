9db580d882286aac7e72d24276d2c54fa87fe31b
Moisés Rocha <moisesrocha99@gmail.com>
2020-03-19 21:58:33 +0000

    tree notation done, arithmetic expressions kinda broken right now

--- javacc/ASTImport.java
+++ javacc/ASTImport.java
@@ -2,7 +2,6 @@
 /* JavaCCOptions:MULTI=true,NODE_USES_PARSER=false,VISITOR=false,TRACK_TOKENS=false,NODE_PREFIX=AST,NODE_EXTENDS=,NODE_FACTORY=,SUPPORT_CLASS_VISIBILITY_PUBLIC=true */
 public
 class ASTImport extends SimpleNode {
-  protected String imported;
 
   public ASTImport(int id) {
     super(id);
