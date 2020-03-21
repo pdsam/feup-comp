ec4c55127b8ee700f6273b10dc10ccdfed651d1e
CajoAlbuquerque <carlosjo.da@gmail.com>
2020-03-21 19:41:44 +0000

    Fixed some errors on import annotations

--- javacc/ASTMethodImport.java
+++ javacc/ASTMethodImport.java
@@ -2,7 +2,7 @@
 /* JavaCCOptions:MULTI=true,NODE_USES_PARSER=false,VISITOR=false,TRACK_TOKENS=false,NODE_PREFIX=AST,NODE_EXTENDS=,NODE_FACTORY=,SUPPORT_CLASS_VISIBILITY_PUBLIC=true */
 public
 class ASTMethodImport extends ASTImport {
-
+  public String returnType;
 
   public ASTMethodImport(int id) {
     super(id);
