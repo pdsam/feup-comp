75da2f7c17d1c93f7490f7f6309d5b130e91156e
pdsam <paulodsam@gmail.com>
2020-06-07 14:57:00 +0100

    going down the rabbit hole

--- src/ConstantPropagation/ConstantPropagationAnalysisVisitor.java
+++ src/ConstantPropagation/ConstantPropagationAnalysisVisitor.java
@@ -4,84 +4,103 @@ import parser.*;
 
 public class ConstantPropagationAnalysisVisitor implements MyGrammarVisitor{
 
+    private void logDebug(String msg){
+        System.out.println("Debug: "+ msg);
+    }
 
     @Override
     public Object visit(SimpleNode node, Object data) {
+        node.childrenAccept(this, data);
         return null;
     }
 
     @Override
     public Object visit(ASTDocument node, Object data) {
+        node.childrenAccept(this, data);
         return null;
     }
 
     @Override
     public Object visit(ASTImportList node, Object data) {
+        node.childrenAccept(this, data);
         return null;
     }
 
     @Override
     public Object visit(ASTImport node, Object data) {
+        node.childrenAccept(this, data);
         return null;
     }
 
     @Override
     public Object visit(ASTClass node, Object data) {
+        node.childrenAccept(this, data);
         return null;
     }
 
     @Override
     public Object visit(ASTVarDeclarationsList node, Object data) {
+        node.childrenAccept(this, data);
         return null;
     }
 
     @Override
     public Object visit(ASTMethodList node, Object data) {
+        node.childrenAccept(this, data);
         return null;
     }
 
     @Override
     public Object visit(ASTMainContainer node, Object data) {
+        node.childrenAccept(this, data);
         return null;
     }
 
     @Override
     public Object visit(ASTVar node, Object data) {
+        node.childrenAccept(this, data);
         return null;
     }
 
     @Override
     public Object visit(ASTMethod node, Object data) {
+        node.childrenAccept(this, data);
         return null;
     }
 
     @Override
     public Object visit(ASTStatementList node, Object data) {
+        node.childrenAccept(this, data);
         return null;
     }
 
     @Override
     public Object visit(ASTReturnStatement node, Object data) {
+        node.childrenAccept(this, data);
         return null;
     }
 
     @Override
     public Object visit(ASTMainMethod node, Object data) {
+        node.childrenAccept(this, data);
         return null;
     }
 
     @Override
     public Object visit(ASTParameter node, Object data) {
+        node.childrenAccept(this, data);
         return null;
     }
 
     @Override
     public Object visit(ASTParameterList node, Object data) {
+        node.childrenAccept(this, data);
         return null;
     }
 
     @Override
     public Object visit(ASTScopedStatementList node, Object data) {
+        node.childrenAccept(this, data);
         return null;
     }
 
@@ -92,6 +111,7 @@ public class ConstantPropagationAnalysisVisitor implements MyGrammarVisitor{
         if(node.value instanceof ASTIntegerLiteral ||node.value instanceof  ASTBooleanLiteral){
             ASTVarReference aux =(ASTVarReference) node.varReference;
             aux.desc.setValue(node.value);
+            this.logDebug("Saving "+aux.identifier+ " for later");
         }
         
 
@@ -101,26 +121,31 @@ public class ConstantPropagationAnalysisVisitor implements MyGrammarVisitor{
 
     @Override
     public Object visit(ASTBranch node, Object data) {
+        node.childrenAccept(this, data);
         return null;
     }
 
     @Override
     public Object visit(ASTWhileLoop node, Object data) {
+        node.childrenAccept(this, data);
         return null;
     }
 
     @Override
     public Object visit(ASTArrayAssignment node, Object data) {
+        node.childrenAccept(this, data);
         return null;
     }
 
     @Override
     public Object visit(ASTExprStatement node, Object data) {
+        node.childrenAccept(this, data);
         return null;
     }
 
     @Override
     public Object visit(ASTArrayAccess node, Object data) {
+        node.childrenAccept(this, data);
         return null;
     }
 
@@ -136,74 +161,85 @@ public class ConstantPropagationAnalysisVisitor implements MyGrammarVisitor{
 
     @Override
     public Object visit(ASTVarReference node, Object data) {
-        
-
-
+        node.childrenAccept(this, data);
         return null;
     }
 
     @Override
     public Object visit(ASTSelfReference node, Object data) {
+        node.childrenAccept(this, data);
         return null;
     }
 
     @Override
     public Object visit(ASTArrayCreation node, Object data) {
+        node.childrenAccept(this, data);
         return null;
     }
 
     @Override
     public Object visit(ASTConstructorCall node, Object data) {
+        node.childrenAccept(this, data);
         return null;
     }
 
     @Override
     public Object visit(ASTNegation node, Object data) {
+        node.childrenAccept(this, data);
         return null;
     }
 
     @Override
     public Object visit(ASTArrayLength node, Object data) {
+        node.childrenAccept(this, data);
         return null;
     }
 
     @Override
     public Object visit(ASTFunctionCall node, Object data) {
+        node.childrenAccept(this, data);
         return null;
     }
 
     @Override
     public Object visit(ASTArguments node, Object data) {
+        node.childrenAccept(this, data);
         return null;
     }
 
     @Override
     public Object visit(ASTAnd node, Object data) {
+        node.childrenAccept(this, data);
         return null;
     }
 
     @Override
     public Object visit(ASTLessThan node, Object data) {
+        node.childrenAccept(this, data);
         return null;
     }
 
     @Override
     public Object visit(ASTSum node, Object data) {
+        node.childrenAccept(this, data);
         return null;
     }
 
     @Override
     public Object visit(ASTSub node, Object data) {
+        node.childrenAccept(this, data);
         return null;
     }
 
     @Override
     public Object visit(ASTMul node, Object data) {
+        node.childrenAccept(this, data);
         return null;
     }
 
     @Override
     public Object visit(ASTDiv node, Object data) {
+        node.childrenAccept(this, data);
         return null;
     }
 }
